bc004c15e2e46c40d52a0fa8ac0b15b8b09b28ad
dezmerek <mszymanski027@gmail.com>
2022-03-02 13:01:02 +0200

    lab1

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -6,7 +6,90 @@ namespace lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = Person.OfName("Adam");
+            Console.WriteLine(person.Name == null ? "NULL" : "PERSON");
+            Money? money = Money.OfWithException(13, Currency.USD) ?? Money.OfWithException(0, Currency.PLN);
+            Console.WriteLine(money.Value);
+            Money result = money*4;
+            Console.WriteLine(result.Value);
+        }
+    }
+
+    public enum Currency
+    {
+        PLN = 1,
+        USD = 2,
+        EUR = 3
+    }
+    public class Money
+    {
+        private readonly decimal _value;
+        private readonly Currency _currency;
+        private Money(decimal value, Currency currency)
+        {
+            _value = value;
+            _currency = currency;
+        }
+        public static Money? OfWithException(decimal value, Currency currency)
+        {
+            return value < 0 ? null : new Money(value, currency);
+        }
+        public static Money operator *(Money money, decimal factor)
+        {
+            return new Money(money.Value * factor, money.Currency);
+        }
+
+        public decimal Value
+        {
+            get { return _value; }
+        }
+
+        public Currency Currency
+        {
+            get { return _currency; }
+
+
+        }
+    }
+
+    class Person
+    {
+        private string _name;
+
+        private Person(string name)
+        {
+            _name = name;
+        }
+
+        public static Person OfName(String name)
+        {
+            if (name != null && name.Length >= 2)
+            {
+                return new Person(name);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Imie zbyt krótkie");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Imie zbyt krótkie");
+                }
+            }
         }
     }
 }
