f3f48c7acfc3ad43b96e167fb304a975d7e0c655
Mateusz Szymański <mszymanski027@gmail.com>
2022-03-19 13:04:57 +0100

    tank

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -2,82 +2,38 @@
 
 namespace lab_1
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Person person = Person.OfName("Mateusz");
-            person.Equals(1);
-            Console.WriteLine(person.Name == null ? "Null" : person.Name);
-            Money money = Money.OfWithException(13, Currency.USD);
-            Console.WriteLine(money.Value);
-            Console.WriteLine(4 * money);
-            Money result = 4 * money;
-            Console.WriteLine(result.Value);
-            Money result2 = money * 4;
-            Console.WriteLine(result.Value);
-            Console.WriteLine(money < Money.OfWithException(5, Currency.USD));
-            //jest to porónanie referencji a nie wartości więc zawsze będzie błędne
-            if (money == Money.OfWithException(13, Currency.USD))
-            {
-                Console.WriteLine("rowne");
-            }
-            else
-            {
-                Console.WriteLine("rozne");
-            }
-            int c = 5;
-            long b = 5;
-            b = c;
-            // c = b; niejawne
-            c = (int)b; //jawne
-            decimal d = money;
-            double price = (double)money;
-            string str = (string)money;
-            Console.WriteLine((string)money);
-            Money[] prices =
-            {
-                Money.OfWithException(10, Currency.USD),
-                Money.OfWithException(13, Currency.PLN),
-                Money.OfWithException(15, Currency.USD),
-                Money.OfWithException(12, Currency.EUR),
-                Money.OfWithException(16, Currency.USD),
-                Money.OfWithException(19, Currency.PLN),
-                Money.OfWithException(11, Currency.EUR),
-            };
-            Array.Sort(prices);
-            foreach (var i in prices)
-            {
-                Console.WriteLine((string)i);
-            }
-        }
-
-
-    }
     public enum Currency
     {
         PLN = 1,
         USD = 2,
         EUR = 3
     }
+
     public class Money : IComparable<Money>
     {
         private readonly decimal _value;
         private readonly Currency _currency;
-
         private Money(decimal value, Currency currency)
         {
             _value = value;
             _currency = currency;
         }
+        public static Money? OfWithException(decimal value, Currency currency)
+        {
 
+            if (value < 0)
+            {
+                throw new ArgumentException("Liczba mniejsza od zera");
+            }
+            else
+            {
+                return new Money(value, currency);
+
+            }
+        }
         public decimal Value
         {
-            get
-            {
-                return _value;
-            }
-
+            get { return _value; }
         }
         public Currency Currency
         {
@@ -86,16 +42,13 @@ namespace lab_1
                 return _currency;
             }
         }
-        public static Money OfWithException(decimal value, Currency currency)
+        public static Money operator *(Money money, decimal factor)
         {
-            if (value > 0)
-            {
-                return new Money(value, currency);
-            }
-            else
-            {
-                throw new ArgumentException("Niepoprawny Obiekt");
-            }
+            return Money.OfWithException(money.Value * factor, money.Currency);
+        }
+        public static Money operator *(decimal factor, Money money)
+        {
+            return Money.OfWithException(money.Value * factor, money.Currency);
         }
 
         public static bool operator >(Money a, Money b)
@@ -103,31 +56,23 @@ namespace lab_1
             IsSameCurrencies(a, b);
             return a.Value > b.Value;
         }
-        public static bool operator <(Money a, Money b)
-        {
-            IsSameCurrencies(a, b);
-            return a.Value < b.Value;
-        }
-        private static object IsSameCurrencies(Money a, Money b)
+
+        private static void IsSameCurrencies(Money a, Money b)
         {
             if (a.Currency != b.Currency)
             {
-                throw new ArgumentException("Currecncy Needs To Be Same");
+                throw new ArgumentException("Inne waluty!");
             }
-            else
-            {
 
-                return a.Value > b.Value;
+        }
+        public static void ToCurrency(Money a, Money b)
+        {
+            decimal result;
+            if (a.Currency == Currency.PLN && b.Currency == Currency.USD)
+            {
+                result = a.Value * 2;
             }
-        }
-        //money 8 4 --> *(money, 4)
-        public static Money operator *(decimal factor, Money money)
-        {
-            return Money.OfWithException(money.Value * factor, money.Currency);
-        }
-        public static Money operator *(Money money, decimal factor)
-        {
-            return Money.OfWithException(money.Value * factor, money.Currency);
+
         }
         public static implicit operator decimal(Money money)
         {
@@ -141,32 +86,95 @@ namespace lab_1
         {
             return $"{money.Value} {money.Currency}";
         }
+
         public override string ToString()
         {
-            return $"{_value}, Currency: {_currency}";
+            return $"Value: {_value}, Currency: {_currency}";
         }
 
-        public int CompareTo(Money other)
+        public int CompareTo(Money? other)
         {
-            int x = Currency.CompareTo(other.Currency);
-            if (x == 0)
+            return Currency.CompareTo(other.Currency);
+            var currencyCon = Currency.CompareTo(other.Currency);
+            if (currencyCon == 0)
             {
                 return Value.CompareTo(other.Value);
             }
             else
             {
-                return x;
+                return currencyCon;
+            }
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            var currencyComparison = _currency.CompareTo(other._currency);
+            if (currencyComparison != 0) return currencyComparison;
+            return _value.CompareTo(other._value);
+        }
+
+        public static bool operator <(Money a, Money b)
+        {
+            IsSameCurrencies(a, b);
+            return a.Value < b.Value;
+        }
+
+    }
+    public class Tank
+    {
+        public readonly float Capacity;
+        private float _level;
+        public Tank(float capacity)
+        {
+            Capacity = (float)capacity;
+        }
+        public float Level
+        {
+            get
+            {
+                return _level;
+            }
+            private set
+            {
+                if (value < 0 || value > Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _level = value;
             }
         }
+        public float refuel(float amount)
+        {
+            if (amount < 0 || _level == Capacity)
+            {
+                return 0;
+            }
+            if (_level + amount > Capacity)
+            {
+                float result = (_level + amount) - Capacity;
+                _level = Capacity;
+                if (result > 0)
+                {
+                    Tank tank2 = new Tank(100);
+                    tank2.refuel(result);
+                    Console.WriteLine("tank 2 = " + tank2.Level);
+                }
+                return result;
+            }
+
+            _level += amount;
+            return amount;
+        }
+
     }
+
     public class Person : IEquatable<Person>
     {
+        private string _name;
+        public int Ects { get; set; }
+        //.//
         private Person(string name)
         {
             _name = name;
         }
-        private string _name;
-        public int ects { get; set; }
         public static Person OfName(String name)
         {
             if (name != null && name.Length >= 2)
@@ -175,32 +183,40 @@ namespace lab_1
             }
             else
             {
-                throw new ArgumentOutOfRangeException("Imię Zbyt Krótkie");
+                throw new ArgumentOutOfRangeException("Imię zbyt krótkie");
             }
         }
+
         public override string ToString()
         {
             return $"Name: {_name}";
         }
-        public bool Equals(Person other)
-        {
-            return other._name.Equals(_name) && other.ects == ects;
-        }
+
         public override bool Equals(object obj)
         {
             return obj is Person person &&
-                   ects == person.ects &&
+                   Ects == person.Ects &&
                    Name == person.Name;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(ects, Name);
+            return HashCode.Combine(Ects, Name);
         }
+
+
+        public bool Equals(Person other)
+        {
+            return other.Name.Equals(_name) && other.Ects == Ects;
+        }
+
 
         public string Name
         {
-            get { return _name; }
+            get
+            {
+                return _name;
+            }
             set
             {
                 if (value != null && value.Length >= 2)
@@ -209,8 +225,51 @@ namespace lab_1
                 }
                 else
                 {
-                    throw new ArgumentOutOfRangeException("Imię jest zbyt krótkie");
+                    throw new ArgumentOutOfRangeException("Imię zbyt krótkie");
                 }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank tank = new Tank(100);
+            Console.WriteLine("tank 1 = " + tank.Level);
+            tank.refuel(10);
+            Console.WriteLine("tank 1 = " + tank.Level);
+            tank.refuel(100);
+            Console.WriteLine("tank 1 = " + tank.Level);
+
+
+            Person person = Person.OfName("Ada");
+            Console.WriteLine(person);
+            Object obj = person;
+            IEquatable<Person> ie = person;
+            Console.WriteLine(person.Name == null ? "null" : "person");
+            Money money = Money.OfWithException(13, Currency.EUR);
+            Console.WriteLine(money.Value);
+            Money result = 4 * money;
+            Console.WriteLine(result.Value);
+            Console.WriteLine(money > Money.OfWithException(5, Currency.EUR));
+            decimal money2 = money;
+            double price = (double)money;
+            string str = (string)money;
+            Console.WriteLine(str);
+            Console.WriteLine("SORT");
+            Money[] pricies =
+            {
+                Money.OfWithException(10,Currency.PLN),
+                Money.OfWithException(8,Currency.USD),
+                Money.OfWithException(12,Currency.EUR),
+                Money.OfWithException(6,Currency.PLN),
+                Money.OfWithException(14,Currency.PLN),
+                Money.OfWithException(3,Currency.PLN),
+            };
+            Array.Sort(pricies);
+            foreach (var p in pricies)
+            {
+                Console.WriteLine((string)p);
             }
         }
     }
